60730695b32604dd54830b30edad598d61e8a74e
Begüm Ircı <begumirci@Begum-MacBook-Air.local>
2024-11-03 14:56:05 +0300

    resim değişti

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className='vista-header'>
-      <img src='/public/vista.jpg' alt='' />
+      <img src='/vista.jpg' alt='' />
       <h1>Coffee Vista</h1>
     </div>
   );
